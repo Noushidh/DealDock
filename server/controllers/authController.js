3f8a2793f2d2c53648deb8687c805ffa6489005a
noushidh <noushidhk@gmail.com>
2026-07-07 18:31:56 +0530

    feat:create register page and implement validation,make types in sepparate folder

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,37 @@
 import User from "../models/userModel.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export const login =async (req,res)=>{
     try{
     const{email,password}=req.body
+
+    const user = await User.findOne({email})
+
+    if(!user){
+      return res.status(401).json({success:false,message:"invalid email or password"})
+    }
     
-    const hashedPassword = await bcrypt.hash(password,10)
+   const isMatch = await bcrypt.compare(password,user.password)
 
-    const user = await User.create({email,password:hashedPassword});
+   if(!isMatch){
+    return res.status(401).json({sucess:false,message:"Invalid email or password"})
+    }
 
-    res.status(201).json({success:true,message:"user registered successfully"})
+    const token = jwt.sign(
+      {
+        id:user._id,
+        email:user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn:"1d"
+      }
+    )
+   console.log("JWT Token:", token);
+
+
+   return res.status(200).json({success:true,message:"Login sucessful",user:{id:user._id,email:user.email},token})
 
     }catch(error){
       res.status(500).json({sucess:false})
@@ -17,4 +39,24 @@ export const login =async (req,res)=>{
 
 }
 
-export default login
+export const register = async (req,res)=>{
+  try{
+      const{email,password}=req.body
+
+    const user = await User.findOne({email})
+
+    if(user){
+      return res.status(409).json({success:false,message:"user is exist"})
+    }
+    
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    await User.create({email,password:hashedPassword});
+
+    res.status(201).json({success:true,message:"user registered successfully"})
+    
+  }catch(error){
+      res.status(500).json({success:false})
+
+  }
+}
--- server/routes/authController.js
+++ server/routes/authController.js
@@ -1,9 +1,10 @@
 
 import express from 'express'
-import login from '../controllers/authController.js';
+import * as Auth from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/login',login)
+router.post('/login',Auth.login)
+router.post('/register',Auth.register)
 
 export default router;
